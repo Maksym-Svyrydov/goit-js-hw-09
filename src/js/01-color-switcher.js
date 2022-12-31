15a4d0e7f23ced622d191296ff05ea4fa69bd1a6
Maksym-Svyrydov <110718671+Maksym-Svyrydov@users.noreply.github.com>
2022-12-31 11:29:59 +0100

    v 1.1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const btnStartEl = document.querySelector('button[data-start]');
+const btnStoptEl = document.querySelector('button[data-stop]');
+const windowEl = document.querySelector('body');
 
+btnStoptEl.disabled = true;
+
+let intrval = null;
+
+btnStartEl.addEventListener('click', onBtnStart);
+btnStoptEl.addEventListener('click', onBtnStop);
+
+function onBtnStart(e) {
+  intrval = setInterval(colorSwitcher, 1000);
+  btnStartEl.disabled = true;
+  btnStoptEl.disabled = false;
+}
+
+function onBtnStop(e) {
+  clearInterval(intrval);
+  btnStartEl.disabled = false;
+  btnStoptEl.disabled = true;
+}
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+function colorSwitcher() {
+  windowEl.style.backgroundColor = getRandomHexColor();
+}
