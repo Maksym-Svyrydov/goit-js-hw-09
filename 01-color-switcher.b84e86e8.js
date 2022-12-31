e34efa6759f3c479fb5a4283573984c8c7da798b
Maksym-Svyrydov <110718671+Maksym-Svyrydov@users.noreply.github.com>
2022-12-31 10:30:34 +0000

    Deploying to gh-pages from @ Maksym-Svyrydov/goit-js-hw-09@15a4d0e7f23ced622d191296ff05ea4fa69bd1a6 🚀

---
+++ 01-color-switcher.b84e86e8.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body");e.disabled=!0;var a=null;function d(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}t.addEventListener("click",(function(n){a=setInterval(d,1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(n){clearInterval(a),t.disabled=!1,e.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.b84e86e8.js.map
